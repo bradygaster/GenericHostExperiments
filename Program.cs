3bdbcc1802ae3dbcf8b3ba52ef4ebe8466355c7d
Brady Gaster <bradyg@microsoft.com>
2018-10-13 01:19:28 -0700

    initial creation

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GenericHostExperiments
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory());
+                    configHost.AddJsonFile("hostsettings.json", optional: true);
+                })
+                .ConfigureAppConfiguration((hostBuilderContext, configApp) =>
+                {
+                    configApp.SetBasePath(Directory.GetCurrentDirectory());
+                    configApp.AddJsonFile("appsettings.json", optional: true);
+                    configApp.AddEnvironmentVariables(prefix: "PREFIX_");
+                })
+                .UseAzureStorage()
+                .Build();
+
+            await host.RunAsync();
+        }
+    }
+}
