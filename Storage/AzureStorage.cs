944f9fe1d3251a9b1f79bb9338076c1412c57013
Brady Gaster <bradyg@microsoft.com>
2018-10-15 22:21:13 -0700

    re-organized classes into better file structure

---
+++ Storage/AzureStorage.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+
+namespace GenericHostExperiments.Storage
+{
+    public class AzureStorage : IHostedService
+    {
+        private readonly IStorageAccountFactory _storageAccountFactory;
+        private IDictionary<string, CloudStorageAccount> _storageAccounts;
+
+        public AzureStorage(IStorageAccountFactory storageAccountFactory, ILogger<AzureStorage> logger)
+        {
+            _storageAccountFactory = storageAccountFactory;
+            Logger = logger;
+        }
+
+        public ILogger Logger { get; }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Logger.LogInformation("AzureStorage started");
+
+            _storageAccounts = _storageAccountFactory.LoadStorageAccounts();
+
+            foreach (var key in _storageAccounts.Keys)
+            {
+                Logger.LogInformation(
+                    string.Format($"Storage Account {key} created with Blob Endpoint {_storageAccounts[key].BlobStorageUri.PrimaryUri}.")
+                );
+            }
+
+            return Task.FromResult(0);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Logger.LogInformation("AzureStorage stopped");
+            return Task.FromResult(0);
+        }
+    }
+}
--- Storage/AzureStorageExtensions.cs
+++ Storage/AzureStorageExtensions.cs
@@ -1,18 +1,12 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Auth;
 
 namespace GenericHostExperiments.Storage
 {
-    public static class StorageAccountExtensions
+    public static class AzureStorageExtensions
     {
         /// <summary>
         /// Builds the list of Azure Storage Accounts defined from configuration.
@@ -50,42 +44,6 @@ namespace GenericHostExperiments.Storage
                 services.AddSingleton<IStorageAccountFactory>(factory);
                 services.AddHostedService<AzureStorage>();
             });
-        }
-    }
-
-    public class AzureStorage : IHostedService
-    {
-        private readonly IStorageAccountFactory _storageAccountFactory;
-        private IDictionary<string, CloudStorageAccount> _storageAccounts;
-
-        public AzureStorage(IStorageAccountFactory storageAccountFactory, ILogger<AzureStorage> logger)
-        {
-            _storageAccountFactory = storageAccountFactory;
-            Logger = logger;
-        }
-
-        public ILogger Logger { get; }
-
-        public Task StartAsync(CancellationToken cancellationToken)
-        {
-            Logger.LogInformation("AzureStorage started");
-
-            _storageAccounts = _storageAccountFactory.LoadStorageAccounts();
-
-            foreach (var key in _storageAccounts.Keys)
-            {
-                Logger.LogInformation(
-                    string.Format($"Storage Account {key} created with Blob Endpoint {_storageAccounts[key].BlobStorageUri.PrimaryUri}.")
-                );
-            }
-
-            return Task.FromResult(0);
-        }
-
-        public Task StopAsync(CancellationToken cancellationToken)
-        {
-            Logger.LogInformation("AzureStorage stopped");
-            return Task.FromResult(0);
         }
     }
 }
